480044dcea936d4e1e49ee36f06ecbfee2f08d5d
estecore <estecore@yandex.ru>
2024-08-12 22:52:43 +0400

    feat(header.tsx): create heaader

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { Container, Title } from "@/components/shared";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <Button variant={"outline"}>Cart</Button>
+      <Container className="mt-10">
+        <Title text="All pizzas" size="lg" className="font-extrabold" />
+      </Container>
     </>
   );
 }
