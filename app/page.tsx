bdfb377c9e27dd28be69afc851447838d4ad31d4
estecore <estecore@yandex.ru>
2024-08-14 20:11:13 +0400

    feat(/): add zustand, create store, create active category

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import {
   Container,
   Filters,
-  ProductCard,
+  ProductsGroupList,
   Title,
   TopBar,
 } from "@/components/shared";
@@ -15,18 +15,222 @@ export default function Home() {
       <TopBar />
 
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           <div className="w-[250px]">
             <Filters />
           </div>
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductCard
-                id="1"
-                name="Hawaiian"
-                price={350}
-                imageUrl="https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif"
+              <ProductsGroupList
+                title="Pizzas"
+                items={[
+                  {
+                    id: "1",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "2",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "3",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "4",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "5",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "6",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "7",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "8",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                ]}
+                categoryId={1}
+              />
+              <ProductsGroupList
+                title="Kombo"
+                items={[
+                  {
+                    id: "1",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "2",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "3",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "4",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "5",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "6",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "7",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "8",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                ]}
+                categoryId={2}
+              />
+              <ProductsGroupList
+                title="Snacks"
+                items={[
+                  {
+                    id: "1",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "2",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "3",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "4",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "5",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "6",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "7",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                  {
+                    id: "8",
+                    name: "Hawaiian",
+                    price: 320,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11EE7D617E9339CFB185921A343AD8FD.avif",
+                    items: [{ price: 320 }],
+                  },
+                ]}
+                categoryId={3}
               />
             </div>
           </div>
