d55e333fae07bf02be09381c596766fcea71d936
estecore <estecore@yandex.ru>
2024-08-16 20:40:27 +0400

    feat(/): create search with url params, create axiosinstance and api constants

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,24 +1,31 @@
+import Link from "next/link";
 import Image from "next/image";
 
 import { ArrowRight, ShoppingCart, User } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
-import { Container } from "./index";
+import { Container, SearchInput } from "./index";
 import { Button } from "../ui";
 
 export const Header = ({ className }: { className?: string }) => {
   return (
     <header className={cn("border border-b", className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="flex items-center gap-4">
-          <Image src={"/logo.png"} alt="logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">
-              it couldn`t be tastier
-            </p>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src={"/logo.png"} alt="logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">
+                it couldn`t be tastier
+              </p>
+            </div>
           </div>
+        </Link>
+
+        <div className="mx-10 flex-1">
+          <SearchInput />
         </div>
 
         <div className="flex items-center gap-3">
