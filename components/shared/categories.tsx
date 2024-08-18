1affc42a938e2f757e0f485a4e0616bc8c92eec6
estecore <estecore@yandex.ru>
2024-08-18 14:53:51 +0400

    feat(homepage, categories.tsx): add fetching categories and products from prisma

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,28 +1,25 @@
 "use client";
 
+import { Category } from "@prisma/client";
+
 import { cn } from "@/lib/utils";
 
 import { useCategoryStore } from "@/store/category";
 
-const cats: { id: number; name: string }[] = [
-  { id: 1, name: "Pizzas" },
-  { id: 2, name: "Kombo" },
-  { id: 3, name: "Snacks" },
-  { id: 4, name: "Cocktails" },
-  { id: 5, name: "Coffee" },
-  { id: 6, name: "Drinks" },
-  { id: 7, name: "Deserts" },
-  { id: 8, name: "Deserts" },
-];
-
-export const Categories = ({ className }: { className?: string }) => {
+export const Categories = ({
+  className,
+  items,
+}: {
+  className?: string;
+  items: Category[];
+}) => {
   const categoryActiveId = useCategoryStore((state) => state.activeId);
 
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map(({ id, name }, index) => (
+      {items.map(({ id, name }, index) => (
         <a
           key={index}
           className={cn(
