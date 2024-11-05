b5e2434c2618237944380d8b130a49b9f26f613c
estecore <estecore@yandex.ru>
2024-11-05 21:17:23 +0400

    feat(/): when success paid useEffect toast

--- shared/hooks/useQueryFilters.ts
+++ shared/hooks/useQueryFilters.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { useRouter } from "next/navigation";
 
@@ -15,23 +15,29 @@ interface Filters {
 }
 
 export const useQueryFilters = (filters: Filters) => {
+  const isMounted = useRef(false);
+
   const router = useRouter();
 
   useEffect(() => {
-    const params = {
-      ...filters.prices,
-      pizzaTypes: Array.from(filters.pizzaTypes),
-      sizes: Array.from(filters.sizes),
-      ingredients: Array.from(filters.selectedIngredients),
-    };
+    if (isMounted.current) {
+      const params = {
+        ...filters.prices,
+        pizzaTypes: Array.from(filters.pizzaTypes),
+        sizes: Array.from(filters.sizes),
+        ingredients: Array.from(filters.selectedIngredients),
+      };
 
-    const query = qs.stringify(params, {
-      arrayFormat: "comma",
-    });
+      const query = qs.stringify(params, {
+        arrayFormat: "comma",
+      });
 
-    router.push(`?${query}`, {
-      scroll: false,
-    });
+      router.push(`?${query}`, {
+        scroll: false,
+      });
+    }
+
+    isMounted.current = true;
   }, [
     filters.pizzaTypes,
     filters.prices,
